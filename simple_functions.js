ac2be88a6217bc51c3c2c40da349f9a0910f40d8
AbelZewdie <Abelmzewdie@gmail.com>
2024-04-29 11:32:31 -0400

    Simple Functions

---
+++ simple_functions.js
@@ -0,0 +1,22 @@
+function favoriteThings() {
+  console.log("My name is Abel")
+  console.log("My favorite movie is The GodFather")
+  console.log("My favorite musician is TheWeeknd")
+}
+
+favoriteThings();
+
+
+console.log("")
+
+function whtImHere() {
+  console.log("Learning to Code")
+}
+whtImHere();
+
+console.log("")
+
+function favoritePlace() {
+  console.log("My favorite place is Ethiopia, cause I was born and raised in there.")
+}
+favoritePlace();
