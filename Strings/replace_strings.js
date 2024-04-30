f48bb0c9d6f10112ef50fbca691aa874dd8027f5
AbelZewdie <Abelmzewdie@gmail.com>
2024-04-30 14:40:12 -0400

    replace strings

---
+++ Strings/replace_strings.js
@@ -0,0 +1,16 @@
+let message = "Our corporate offices are located in Dallas, and our retail store is also in dallas and our mfg facility is also in Dallas";
+
+// Replace dallace with Austin
+let updated = message.replace(/Dallas/gi, "Austin"); 
+//g is global all around
+//i is insensitive to case so upper and lower case (capital)
+
+
+
+console.log(updated)
+
+// for(let  i = 0; mc=0; mc < 2 && i < WebTransportDatagramDuplexStream.length; i++){
+//   if (word == sought)(
+//     mc++;
+//   )
+// }
